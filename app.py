848150bad36d59ff3c20486fb0a90ea4c901bd52
Tuchanski <gui.rocha2004@gmail.com>
2025-08-08 13:28:47 -0300

    Refactor app

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template, request
 import ollama
 
-app = Flask(__name__)
-
-def generate_response(prompt: str, model="llama3.1:8b") -> str:
+def generate_response(model="llama3.1:8b") -> str:
+    prompt = prompt = "Você está respondendo sua namorada. Seja o mais genérico possível. Envie uma mensagem fofa."
     response = ollama.chat(model=model, messages=[{'role': 'user', 'content': prompt}])
     return response['message']['content'].replace("\"", "")
 
@@ -11,10 +10,11 @@ def send_whatsapp_message(number_target: str, message: str):
     import pywhatkit
     pywhatkit.sendwhatmsg_instantly(number_target, message)
 
+app = Flask(__name__)
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     response = None
-    prompt = "Você está respondendo sua namorada. Seja o mais genérico possível. Envie uma mensagem fofa."
     numero = ""
 
     if request.method == "POST":
@@ -22,8 +22,8 @@ def index():
         numero = request.form.get("numero")
 
         if action == "generate":
-            response = generate_response(prompt)
-        elif action == "send":
+            response = generate_response()
+        else:
             response = request.form.get("response")
             send_whatsapp_message(numero, response)
 
