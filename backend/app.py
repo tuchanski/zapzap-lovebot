49f0d18e3294d481715242e88850b72e490bc0c6
Tuchanski <gui.rocha2004@gmail.com>
2025-09-03 16:35:26 -0300

    Update backend

--- backend/app.py
+++ backend/app.py
@@ -1,67 +1,67 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import os
 from groq import Groq
 
+import os
+
 app = Flask(__name__)
-CORS(app)
+client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
 
-tags = [
-    "fofo", "apaixonado", "saudade",
-      "reconfortar", "bom-dia", "boa-noite",
-        "aleatorio", "emoji"]
+CORS(app) # Libera rotas pro front se comunicar com o back
 
-client = Groq(
-    api_key=os.environ.get("GROQ_API_KEY"),
-)
+# Core do Programa
 
 def generate_response(tags: list[str] = None) -> str:
-
     tags_text = ""
 
     if tags:
          tags_text = " ".join([f"[{tag}]" for tag in tags])
 
     prompt = (
-        f"Você está respondendo sua namorada. "
-        f"Seja o mais genérico possível. Envie uma mensagem fofa. "
+        f"Máximo 60 palavras."
+        f"Você mandará uma mensagem fofa para sua namorada."
+        f"Seja o mais genérico possível."
         f"{tags_text}"
     )
 
     chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": prompt,
-        }
-    ],
-        model="llama-3.3-70b-versatile",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ], model="llama-3.3-70b-versatile",
     )
+
     return chat_completion.choices[0].message.content.replace("\"", "")
 
 def send_whatsapp_message(number_target: str, message: str):
     import pywhatkit
-    number_target_dict = ""
-    response_dict = ""
+
+    number_target_filtered = ""
+    response_filtered = ""
 
     for key, value in number_target.items():
-        number_target_dict = value
+        number_target_filtered = value
 
     for key, value in message.items():
-        response_dict = value
+        response_filtered = value
     
     try:
-        pywhatkit.sendwhatmsg_instantly(number_target_dict, response_dict)
+        pywhatkit.sendwhatmsg_instantly(number_target_filtered, response_filtered)
+        print("Mensagem enviada com sucesso. <3")
+
     except Exception as e:
-        print(e)
+        print("Erro ao enviar mensagem: ", e)
+
+# Rotas
 
 @app.route("/generate", methods=["GET"])
 def generate():
     tags_param = request.args.get("tags")
     tags_list = tags_param.split(",") if tags_param else []
     print(tags_list)
-    valid_tags = [tag for tag in tags_list if tag in tags]
-    return jsonify({"response": generate_response(valid_tags)})
+    return jsonify({"response": generate_response(tags_list)})
 
 @app.route("/send", methods=["POST"])
 def send():
