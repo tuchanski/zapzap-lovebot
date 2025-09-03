3a009e2bbea7d80612d54a978a18482a2367e66b
Tuchanski <gui.rocha2004@gmail.com>
2025-09-03 14:29:38 -0300

    Update api to app & Implement options

--- backend/app.py
+++ backend/app.py
@@ -6,16 +6,33 @@ from groq import Groq
 app = Flask(__name__)
 CORS(app)
 
+tags = [
+    "fofo", "apaixonado", "saudade",
+      "reconfortar", "bom-dia", "boa-noite",
+        "aleatorio", "emoji"]
+
 client = Groq(
     api_key=os.environ.get("GROQ_API_KEY"),
 )
 
-def generate_response() -> str:
+def generate_response(tags: list[str] = None) -> str:
+
+    tags_text = ""
+
+    if tags:
+         tags_text = " ".join([f"[{tag}]" for tag in tags])
+
+    prompt = (
+        f"Você está respondendo sua namorada. "
+        f"Seja o mais genérico possível. Envie uma mensagem fofa. "
+        f"{tags_text}"
+    )
+
     chat_completion = client.chat.completions.create(
     messages=[
         {
             "role": "user",
-            "content": "Você está respondendo sua namorada. Seja o mais genérico possível. Envie uma mensagem fofa.",
+            "content": prompt,
         }
     ],
         model="llama-3.3-70b-versatile",
@@ -40,7 +57,11 @@ def send_whatsapp_message(number_target: str, message: str):
 
 @app.route("/generate", methods=["GET"])
 def generate():
-    return jsonify({"response": generate_response()})
+    tags_param = request.args.get("tags")
+    tags_list = tags_param.split(",") if tags_param else []
+    print(tags_list)
+    valid_tags = [tag for tag in tags_list if tag in tags]
+    return jsonify({"response": generate_response(valid_tags)})
 
 @app.route("/send", methods=["POST"])
 def send():
